c630f7eed574deab0dcc0f8f763adcfd3d68f2c0
Danil Prokopenko <69258218+prokodan@users.noreply.github.com>
2023-05-12 14:33:11 +0600

    Initial Commit

---
+++ Geographic atlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geographic atlas
+//
+//  Created by Данил Прокопенко on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
